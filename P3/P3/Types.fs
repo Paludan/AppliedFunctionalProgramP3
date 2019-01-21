eb4d2b0d75349359c268072e37425cf1187013bf
Thomas <s153047@student.dtu.dk>
2019-01-21 12:20:21 +0100

    inital, with types

---
+++ P3/P3/Types.fs
@@ -0,0 +1,41 @@
+
+type Student = {
+    name: string
+    ID: int
+    study: string
+}
+
+and Group = {
+    ID: int
+    members: Student list 
+    projectPriorities: Project list
+}
+
+and Teacher = {
+    name: string 
+    department: string
+}
+
+and Project = {
+    title: string
+    description: string
+    supervisor: Teacher 
+    coSupervisor: Teacher list option
+    limits: Limitations
+}
+
+and Limitations = {
+    prereq: string list option
+    maxSize: int option 
+    maxGroups: int option
+}
+
+and ProjectDatabase = {
+    study: string
+    projectMap: Map<int, Project>
+}
+
+and HeadOfStudies = {
+    name: string 
+    study: string
+}
