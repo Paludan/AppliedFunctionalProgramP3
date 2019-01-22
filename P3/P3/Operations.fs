76938896621b2ba5f5044d4d423733955a696253
Lee Paludan <Leepaludan@gmail.com>
2019-01-22 15:41:42 +0100

    Made records immutable + addproposed func

--- P3/P3/Operations.fs
+++ P3/P3/Operations.fs
@@ -2,11 +2,11 @@ namespace P3
 
 open P3.Types
 
-module Operations =
-
+module Operations =    
     let generateId () = (System.DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff",
                                                      System.Globalization.CultureInfo.InvariantCulture)
                         ).GetHashCode()
 
-    let proposeProject (p : Project) (m : Map<(string * int), Project>) : Map<(string * int), Project> =
-        m.Add((p.projectName, generateId()), p)
+
+    let proposeProject (p : Project) (study : Study) =
+        study.ProposedMap <- study.ProposedMap.Add(generateId(), p)
--- P3/P3/Types.fs
+++ P3/P3/Types.fs
@@ -2,47 +2,47 @@ namespace P3
 
 module Types =
     type Student = {
-        name: string
+        mutable name: string
     }
 
     and Group = {
-        GID: int
-        members: Student list
-        projectPriorities: Project list
+        mutable GID: int
+        mutable members: Student list
+        mutable projectPriorities: Project list
     }
 
     and Teacher = {
-        name: string
-        department: string
+        mutable name: string
+        mutable department: string
     }
 
     and Project = {
-        projectName: string
-        description: string
-        supervisor: Teacher
-        coSupervisor: Teacher list option
-        Groups: Group list option
-        limits: Limitations
+        mutable projectName: string
+        mutable description: string
+        mutable supervisor: Teacher
+        mutable coSupervisor: Teacher list option
+        mutable Groups: Group list option
+        mutable limits: Limitations
     }
 
     and Limitations = {
-        prereq: string list option
-        maxSize: int option
-        maxGroups: int option
+        mutable prereq: string list option
+        mutable maxSize: int option
+        mutable maxGroups: int option
     }
 
     and ProjectDatabase = {
-        projectMap: Map<(int * string), Project>
+        mutable projectMap: Map<(int * string), Project>
     }
 
     and HeadOfStudies = {
-        teacher: Teacher
+        mutable teacher: Teacher
     }
-    
+
     and Study = {
-        AcceptedMap: Map<int, Project> // Projects accepted (PID -> Project)
-        ProposedMap: Map<int, Project> // Projects proposed (PID -> Project)
-        StudentMap:  Map<int, Student> // Student database (SID -> Student)
-        Groups:      Map<int, Group>   // Groups (GID -> Group)
-        Head:        HeadOfStudies
+        mutable AcceptedMap: Map<int, Project> // Projects accepted (PID -> Project)
+        mutable ProposedMap: Map<int, Project> // Projects proposed (PID -> Project)
+        mutable StudentMap:  Map<int, Student> // Student database (SID -> Student)
+        mutable Groups:      Map<int, Group>   // Groups (GID -> Group)
+        mutable Head:        HeadOfStudies
     }
