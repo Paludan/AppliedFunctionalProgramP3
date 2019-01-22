1b12f69bbadaa9b1d1c46a0ab9fa9abf0de97d08
Lee Paludan <Leepaludan@gmail.com>
2019-01-22 16:39:15 +0100

    SDK version changed + Added limit checker for projects

--- P3/P3/Operations.fs
+++ P3/P3/Operations.fs
@@ -7,6 +7,24 @@ module Operations =
                                                      System.Globalization.CultureInfo.InvariantCulture)
                         ).GetHashCode()
 
+    let handleProposedProject pId (study : Study) judgement =
+            match judgement with
+            | true  -> match study.ProposedMap.TryFind pId with
+                       | Some x -> 
+                           study.ProposedMap <- study.ProposedMap.Remove pId
+                           study.AcceptedMap <- study.AcceptedMap.Add(pId, x)
+                       | None   -> failwith "Project not in database!"
+            | false ->
+                study.ProposedMap <- study.ProposedMap.Remove pId
 
     let proposeProject (p : Project) (study : Study) =
         study.ProposedMap <- study.ProposedMap.Add(generateId(), p)
+
+    let checkLimits (p : Project) : bool =
+        match p.Groups with
+        | Some gs -> gs.Length = (List.filter (fun (x : Group) -> 
+                                    match p.limits.maxSize with
+                                    | Some s -> x.members.Length <= s
+                                    | None   -> true
+                                ) gs).Length
+        | None   -> true
