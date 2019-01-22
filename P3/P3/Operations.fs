1ad398fb41a7bbcdd054b066230c0cb5310072cb
Thomas <s153047@student.dtu.dk>
2019-01-22 16:40:39 +0100

    reviewGroups function

--- P3/P3/Operations.fs
+++ P3/P3/Operations.fs
@@ -10,3 +10,10 @@ module Operations =
 
     let proposeProject (p : Project) (study : Study) =
         study.ProposedMap <- study.ProposedMap.Add(generateId(), p)
+    
+    let reviewGroups (study : Study) = 
+        let groupList = Map.fold (fun acc _ g -> List.append acc [(g.GID, List.fold (fun acc (m : Student) -> acc @ [m.name]) [] g.members, List.fold (fun acc p -> acc @ [p.projectName]) [] g.projectPriorities)] ) [] study.Groups
+        List.iter (fun (id,ml,pl) -> printfn "Group %i: \n\tMembers: %s \n\tProjects: %s"id (String.concat ", " ml) (String.concat ", " pl) ) groupList
+        |> ignore
+        //let memberList = List.fold (fun acc ) [] groupList
+    //    Map.fold (fun acc id g -> acc + "%s " (List.fold (fun acc stud -> acc + stud.sname) "" g.members) ) "" study.G
