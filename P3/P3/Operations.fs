866e7254727e12c1848d7ed28fb280ffbf394c71
Thomas <s153047@student.dtu.dk>
2019-01-23 16:44:02 +0100

    functions to modify assignedProject

--- P3/P3/Operations.fs
+++ P3/P3/Operations.fs
@@ -58,3 +58,62 @@ module Operations =
                                     AcceptedMap = study.AcceptedMap.Add(pId, { p with Groups = Some [g] })
                                 }
         | None -> failwith "Project not in Database!"
+
+    let lookupGroupPriorities group study =
+        let projList = List.map (fun pid -> study.AcceptedMap.TryFind pid) group.projectPriorities
+        let projNameList = List.map (
+                                fun pOpt -> 
+                                match pOpt with
+                                | Some(p) -> p.projectName
+                                | None -> "Project not assigned as an accepted project"
+                           ) projList
+        
+        printfn "Group %i:" group.GID
+        printfn "\tPriorities: %s" (String.concat ", " projNameList)
+        |> ignore
+
+    let unassignCurrentProject group study = 
+        // find previously assigned projects if any
+        let prevAssignedProj = Map.fold (fun acc k (p : Project) -> 
+                                        match p.Groups with
+                                        | Some(gList) when (List.exists (fun g -> g.GID = group.GID) gList) -> (k,p) :: acc
+                                        | _ ->  acc
+                                      ) [] study.AcceptedMap
+        // remove the group form previous project
+        let prevAssignedProj = List.map (fun (k,(p : Project)) -> 
+                                            match p.Groups with 
+                                            | Some(gList) -> 
+                                                let gList' = List.filter (fun g -> g.GID <> group.GID) gList
+                                                let gOpt' = if (List.isEmpty gList') then None else Some(gList')
+                                                (k,{p with Groups = gOpt'})
+                                        ) prevAssignedProj
+        prevAssignedProj
+
+
+    let assignToNewProj group projID study =
+        let newProj = match study.AcceptedMap.TryFind projID with 
+                      | Some(proj) -> proj
+                      | None -> failwith "Project does not exists"
+        // add group to new project
+        let newProj' = match newProj.Groups with 
+                       | Some(gList) -> 
+                            let gList' = group :: gList 
+                            let gOpt' = if (List.isEmpty gList') then None else Some(gList')
+                            {newProj with Groups = gOpt'}
+                       | None -> {newProj with Groups = Some([group])}
+        newProj'
+
+    let assignGroup group projID study = 
+        let prevAssignedProj = unassignCurrentProject group study
+        
+        let newProj = assignToNewProj group projID study
+
+        let newAcceptedMap = List.fold (fun mapPrev list -> 
+                                Map.remove (fst list) mapPrev 
+                                |> Map.add (fst list) (snd list)
+                             ) study.AcceptedMap prevAssignedProj
+                             |> Map.remove projID 
+                             |> Map.add projID newProj
+        // return study with changed assignment
+        let tmpStudy = {study with AcceptedMap = newAcceptedMap}
+        tmpStudy
